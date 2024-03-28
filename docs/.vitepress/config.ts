9b3b2a2601833aff8c4ef5714a9721d3837ae6ab
huaerss <969742563@qq.com>
2024-03-28 11:45:02 +0800

    init

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: 'dist',
-  base: '/',
+  base: './',
 
   // base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
   lang: 'zh-CN',
